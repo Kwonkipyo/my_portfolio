87d92bb91d7e54c4ea7aee74e9e2ee3d82b712f5
Kwonkipyo <k>
2023-08-31 10:47:30 +0900

    feat: profile 파트 수정

--- js/particles.js
+++ js/particles.js
@@ -1,35 +1,38 @@
 $("document").ready(function () {
   particlesJS("particles-js", {
     particles: {
-      number: { value: 400, density: { enable: true, value_area: 800 } },
-      color: { value: "#fff" },
+      number: {
+        value: 28,
+        density: { enable: true, value_area: 4655.971085630409 },
+      },
+      color: { value: "#a2e3f7" },
       shape: {
         type: "circle",
-        stroke: { width: 0, color: "#000000" },
-        polygon: { nb_sides: 5 },
+        stroke: { width: 0, color: "#000" },
+        polygon: { nb_sides: 6 },
         image: { src: "img/github.svg", width: 100, height: 100 },
       },
       opacity: {
-        value: 0.5,
+        value: 0.26041872173865,
         random: true,
         anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
       },
       size: {
-        value: 10,
-        random: true,
-        anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+        value: 94.69771699587272,
+        random: false,
+        anim: { enable: true, speed: 10, size_min: 40, sync: false },
       },
       line_linked: {
         enable: false,
-        distance: 500,
+        distance: 200,
         color: "#ffffff",
-        opacity: 0.4,
+        opacity: 1,
         width: 2,
       },
       move: {
         enable: true,
-        speed: 6,
-        direction: "bottom",
+        speed: 8,
+        direction: "none",
         random: false,
         straight: false,
         out_mode: "out",
@@ -40,13 +43,13 @@ $("document").ready(function () {
     interactivity: {
       detect_on: "canvas",
       events: {
-        onhover: { enable: true, mode: "bubble" },
-        onclick: { enable: true, mode: "repulse" },
+        onhover: { enable: false, mode: "grab" },
+        onclick: { enable: false, mode: "push" },
         resize: true,
       },
       modes: {
-        grab: { distance: 400, line_linked: { opacity: 0.5 } },
-        bubble: { distance: 400, size: 4, duration: 0.3, opacity: 1, speed: 3 },
+        grab: { distance: 400, line_linked: { opacity: 1 } },
+        bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
         repulse: { distance: 200, duration: 0.4 },
         push: { particles_nb: 4 },
         remove: { particles_nb: 2 },
